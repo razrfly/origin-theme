2e5c8c6ef844c9fb85f47314874504a9286d74da
holden <holden@razrfly.com>
2023-09-20 12:14:23 +0200

    change image quality

--- modules/swell/components/VisualMedia.js
+++ modules/swell/components/VisualMedia.js
@@ -54,7 +54,7 @@ export default {
     // Higher value = less compression, better quality, larger file size
     quality: {
       type: Number,
-      default: 80,
+      default: 95,
     },
     // Image widths to generate source urls for
     widths: {
